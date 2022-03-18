f1edeaaf195076994bb0e3af61b9de431a104565
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2022-03-19 01:50:30 +0800

    added feature: add and delete books

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc
+} from 'firebase/firestore'
+import firebaseConfig from './env'
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-}
-
+// init firebase app
 initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+  .then(snapshot => {
+    let books = []
+    console.log('snapshot docs', snapshot.docs)
+    snapshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', e => {
+  e.preventDefault()
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+  deleteDoc(docRef)
+  .then(() => {
+    deleteBookForm.reset()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    watch: true
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false
+    }
+  },
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  watch: true
 }
